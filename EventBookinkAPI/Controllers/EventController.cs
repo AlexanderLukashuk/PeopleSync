2254af7ce87938610927e809a380fa6468adf257
AlexanderLukashuk <upiter465@gmail.com>
2025-03-20 05:23:43 +0500

    add auth check and organizer check for create method

--- EventBookinkAPI/Controllers/EventController.cs
+++ EventBookinkAPI/Controllers/EventController.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 using EventBookinkAPI.Data;
 using EventBookinkAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +18,7 @@ public class EventController : ControllerBase
         this.context = context;
     }
 
+    [Authorize]
     [HttpGet]
     public async Task<IActionResult> GetEvents()
     {
@@ -23,11 +26,35 @@ public class EventController : ControllerBase
         return Ok(events);
     }
 
+    [Authorize]
     [HttpPost]
     public async Task<IActionResult> CreateEvent(Event ev)
     {
+        var appMetadataClaim = User.Claims.FirstOrDefault(c => c.Type == "app_metadata");
+        // var userRole = appMetadataClaim?.Value;
+        // Console.WriteLine($"User Role: {userRole}");
+        Console.WriteLine($"AppMetadata: {appMetadataClaim?.Value}");
+
+        foreach (var claim in User.Claims)
+        {
+            Console.WriteLine($"Claim Type: {claim.Type}, Value: {claim.Value}");
+        }
+
+        if (appMetadataClaim == null)
+        {
+            return Forbid();
+        }
+
+        var appMetadata = JsonSerializer.Deserialize<Dictionary<string, object>>(appMetadataClaim.Value);
+        var userRole = appMetadata.TryGetValue("role", out var roleValue) ? roleValue?.ToString() : null;
+
+        if (userRole != "organizer")
+        {
+            return Forbid();
+        }
+
         context.Events.Add(ev);
         await context.SaveChangesAsync();
-        return CreatedAtAction(nameof(GetEvents), new { id = ev.id}, ev);
+        return CreatedAtAction(nameof(GetEvents), new { id = ev.id }, ev);
     }
 }
